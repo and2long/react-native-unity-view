b16726a08237eb132523a2f5079097f3c2a21fc1
Lilong Zhang <and2long@gmail.com>
2023-07-03 15:17:42 +0800

    upgrade to 2022.3.4f1

--- example/unity/example/Assets/Scripts/Editor/Build.cs
+++ example/unity/example/Assets/Scripts/Editor/Build.cs
@@ -18,7 +18,7 @@ public class Build : MonoBehaviour {
     private static readonly string iosExportPath =
         Path.GetFullPath(Path.Combine(ProjectPath, "../../ios/UnityExport"));
 
-    [MenuItem("ReactNative/Export Android (Unity 2019.4.*) %&n", false, 1)]
+    [MenuItem("ReactNative/Export Android (Unity 2022.3.*) %&n", false, 1)]
     public static void DoBuildAndroidLibrary() {
         DoBuildAndroid(Path.Combine(apkPath, "unityLibrary"));
         
@@ -40,7 +40,7 @@ public class Build : MonoBehaviour {
 
         EditorUserBuildSettings.androidBuildSystem = AndroidBuildSystem.Gradle;
 
-        var options = BuildOptions.AcceptExternalModificationsToPlayer;
+        var options = BuildOptions.AllowDebugging;
         var report = BuildPipeline.BuildPlayer(
             GetEnabledScenes(),
             apkPath,
@@ -73,13 +73,13 @@ public class Build : MonoBehaviour {
         File.WriteAllText(manifest_file, manifest_text);
     }
 
-    [MenuItem("ReactNative/Export IOS (Unity 2019.4.*) %&i", false, 3)]
+    [MenuItem("ReactNative/Export IOS (Unity 2022.3.*) %&i", false, 3)]
     public static void DoBuildIOS() {
         if (Directory.Exists(iosExportPath)) {
             Directory.Delete(iosExportPath, true);
         }
 
-        EditorUserBuildSettings.iOSBuildConfigType = iOSBuildType.Release;
+        EditorUserBuildSettings.iOSXcodeBuildConfig = XcodeBuildConfig.Release;
 
         var options = BuildOptions.AcceptExternalModificationsToPlayer;
         var report = BuildPipeline.BuildPlayer(
